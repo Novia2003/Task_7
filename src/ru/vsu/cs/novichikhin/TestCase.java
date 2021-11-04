b4d559fb19d6e1b6102f935cdd4b296b8c0d5bf0
Novia2003 <90568044+Novia2003@users.noreply.github.com>
2021-11-04 21:45:19 +0300

    Add files via upload

---
+++ src/ru/vsu/cs/novichikhin/TestCase.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.novichikhin;
+
+public class TestCase {
+    public int[] array;
+    public int n;
+    public int result;
+
+    public TestCase(int[] array, int n, int result) {
+        this.array = array;
+        this.n = n;
+        this.result = result;
+    }
+}
---
+++ src/ru/vsu/cs/novichikhin/TestResult.java
@@ -0,0 +1,14 @@
+package ru.vsu.cs.novichikhin;
+
+public class TestResult {
+    public boolean result;
+
+    public TestResult(boolean result) {
+        this.result = result;
+    }
+
+    public boolean getTestResult() {
+        return result;
+    }
+}
+
