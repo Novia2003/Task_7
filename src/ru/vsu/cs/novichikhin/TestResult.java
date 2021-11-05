a9c902d098da546a764762628394229fda74678c
Novia2003 <90568044+Novia2003@users.noreply.github.com>
2021-11-05 11:02:15 +0300

    Add files via upload

---
+++ src/ru/vsu/cs/novichikhin/TestResult.java
@@ -0,0 +1,26 @@
+package ru.vsu.cs.novichikhin;
+
+public class TestResult {
+    boolean isProgramCorrect;
+    int[] array;
+    int n;
+
+    public TestResult(boolean isProgramCorrect, int[] array, int n) {
+        this.isProgramCorrect = isProgramCorrect;
+        this.array=array;
+        this.n = n;
+    }
+
+    public boolean GetIsProgramCorrect() {
+        return isProgramCorrect;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public int getN() {
+        return n;
+    }
+}
+
